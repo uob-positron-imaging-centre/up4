7735dafbee5599917932db8f54457c51cb07d82f
Dominik Werner <dxw963@student.bham.ac.uk>
2020-12-02 12:20:20 +0000

    Improved liggghts reader

--- pyAnalyser/converter.py
+++ pyAnalyser/converter.py
@@ -6,7 +6,7 @@ from vtk import vtkDataSetReader
 from vtk.util.numpy_support import vtk_to_numpy
 from natsort import natsorted, ns
 import os
-
+from glob import glob
 def PEPT(filename,header = 16):
     data = np.genfromtxt(filename,skip_header=header)
     dump = 2
@@ -17,7 +17,7 @@ def PEPT(filename,header = 16):
         if os.path.exists(filename2):
             print(f"Appending data {filename} with {filename2}")
             data2 = np.genfromtxt(filename2,skip_header=header)
-            data= np.concatenate((data,data2)) 
+            data= np.concatenate((data,data2))
             dump+=1
         else:
             break
@@ -29,8 +29,8 @@ def PEPT(filename,header = 16):
     max_val=np.asarray([-inf,-inf,-inf])
     ###
     #needed variables
-    
-    
+
+
     velocitys=[]
     positions=[]
     time = []
@@ -40,8 +40,8 @@ def PEPT(filename,header = 16):
             continue
 
         time.append( timestep[0])
-       
-        
+
+
         x = timestep[1]
         y = timestep[3]
         z = timestep[2]
@@ -54,76 +54,86 @@ def PEPT(filename,header = 16):
         velocity = np.asarray([vx,vy,vz])
         positions.append(position)
         velocitys.append(velocity)
-        
+
         min_mask = position < min_val
         max_mask = position > max_val
         min_val[min_mask] = position[min_mask]
         max_val[max_mask]=position[max_mask]
-         
+
     cur_step = 0
-    
+
     print(np.asarray(positions).shape)
     print(np.asarray(positions[0:1]).shape)
-    
+
     print(np.asarray(positions[0]).shape)
     grp = file.create_group("timestep "+str(cur_step))
-    grp.create_dataset("time", data = time[0])        
+    grp.create_dataset("time", data = time[0])
     grp.create_dataset("position",data = np.asarray(positions))
     grp.create_dataset("velocity",data = np.asarray(velocitys))
     grp.create_dataset("radius",data = np.asarray([0]))
     grp.create_dataset("ppcloud",data = np.asarray([0]))
     grp.create_dataset("spezies",data = np.asarray([0]))
     grp.create_dataset("particleid",data = np.asarray([0]))
-        
-    grp = file.create_group("timestep "+str(1))   
-    grp.create_dataset("time", data = time[10])         
-            
-            
+
+    grp = file.create_group("timestep "+str(1))
+    grp.create_dataset("time", data = time[10])
+
+
     file.create_dataset("dimensions", data = np.asarray([min_val,max_val]))
 
-def Liggghts(filenames,dt = 1):
-    filenames = natsorted(filenames,key=lambda y: y.lower()) 
+
+def Liggghts(files, dt=1):
+    # filetype check.
+    # we accept the path to files or a list of files
+    if type(files) == type([]):
+        files = [f for f in files if f.endswith("vtk")]
+        if len(files) < 1:
+            raise ValueError(("Files supplyed don't have the accepted "
+                "VTK format!"))
+    elif isinstance(files,str):
+        files = [x for x in  glob(files+"*.vtk") if not "bound" in x]
+        if len(files) < 1:
+            raise ValueError("No files found in this directory")
+    filenames = natsorted(files,key=lambda y: y.lower())
     inf=float("inf")
     min_val=np.asarray([inf,inf,inf])
     max_val=np.asarray([-inf,-inf,-inf])
-    file = h5py.File("_".join(filenames[0].split("_")[0:-1])+".hdf5","w")
-    for id,filename in enumerate(filenames):
-        #create group for current timestep
-        grp = file.create_group("timestep "+str(id))
-        ##### read the ligghts file
-        reader = vtkDataSetReader()
-        reader.SetFileName(filename)
-        reader.ReadAllScalarsOn() 
-        reader.ReadAllVectorsOn() 
-        reader.Update()
-        data = reader.GetOutput()
-        p = data.GetPointData()
-        
-        time = int(filename.split(".vtk")[0].split("_")[-1]) * dt
-        position = vtk_to_numpy(data.GetPoints().GetData())
-        velocity = vtk_to_numpy(p.GetArray("v")) 
-        ID = vtk_to_numpy(p.GetArray("id"))
-        radius = vtk_to_numpy(p.GetArray("radius"))
-        type_ = vtk_to_numpy(p.GetArray("type"))
-        
-        
-        # create datasets for each variables
-        grp.create_dataset("time", data = time)
-        grp.create_dataset("position",data = position) 
-        grp.create_dataset("velocity",data = velocity )
-        grp.create_dataset("radius",data = radius )
-        grp.create_dataset("ppcloud",data = np.zeros(len(type_)) )
-        grp.create_dataset("spezies",data = type_ )
-        grp.create_dataset("particleid",data = ID )
-        
-        
-        #checking the min and ma position
-        pos_min = np.min(position,axis = 0)
-        pos_max = np.max(position,axis = 0)
-        min_ = pos_min < min_val
-        max_ = pos_max > max_val
-        min_val[min_] = pos_min[min_] 
-        max_val[max_] = pos_max[max_] 
-    file.create_dataset("dimensions", data = np.asarray([min_val,max_val]))  
-    file.close()
-        
+    with h5py.File("_".join(filenames[0].split("_")[0:-1])+".hdf5","w") as file:
+        for id,filename in enumerate(filenames):
+            #create group for current timestep
+            grp = file.create_group("timestep "+str(id))
+            ##### read the ligghts file
+            reader = vtkDataSetReader()
+            reader.SetFileName(filename)
+            reader.ReadAllScalarsOn()
+            reader.ReadAllVectorsOn()
+            reader.Update()
+            data = reader.GetOutput()
+            p = data.GetPointData()
+
+            time = int(filename.split(".vtk")[0].split("_")[-1]) * dt
+            position = vtk_to_numpy(data.GetPoints().GetData())
+            velocity = vtk_to_numpy(p.GetArray("v"))
+            ID = vtk_to_numpy(p.GetArray("id"))
+            radius = vtk_to_numpy(p.GetArray("radius"))
+            type_ = vtk_to_numpy(p.GetArray("type"))
+
+
+            # create datasets for each variables
+            grp.create_dataset("time", data = time)
+            grp.create_dataset("position",data = position)
+            grp.create_dataset("velocity",data = velocity )
+            grp.create_dataset("radius",data = radius )
+            grp.create_dataset("ppcloud",data = np.zeros(len(type_)) )
+            grp.create_dataset("spezies",data = type_ )
+            grp.create_dataset("particleid",data = ID )
+
+
+            #checking the min and ma position
+            pos_min = np.min(position,axis = 0)
+            pos_max = np.max(position,axis = 0)
+            min_ = pos_min < min_val
+            max_ = pos_max > max_val
+            min_val[min_] = pos_min[min_]
+            max_val[max_] = pos_max[max_]
+        file.create_dataset("dimensions", data = np.asarray([min_val,max_val]))

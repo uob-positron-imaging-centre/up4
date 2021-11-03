901994e2d0ca7d9c0bf24df81c3dd389248df8f5
Dominik Werner <d.wer2@gmx.de>
2021-11-03 16:34:07 +0000

    minor updates

--- src/base/converter.rs
+++ src/base/converter.rs
@@ -1,3 +1,13 @@
-fn main(){
-    println!("Hello Work! :'-)")
+//! This file provides functionalities to convert data to HDF5 file format
+//!
+//!
+
+
+pub fn vtk(
+    filenames: Vec<&str>,
+    timestep: f64,
+    outname: &str,
+
+){
+
 }

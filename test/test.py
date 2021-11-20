9c47de13687a9c7a83b10ef2778390fa478678e7
Dominik Werner <d.wer2@gmx.de>
2021-11-20 16:59:01 +0000

    base layout of testing

--- test/test.py
+++ test/test.py
@@ -1,41 +1,69 @@
+#! /usr/bin/python
+# Author:   Dominik Werner
+# File:     test.py
+# Date:     20.11.21
+
+###############
+# Testing suit for uPPPP
+# this skript tests given functions and returns its speed and
+# if it failed or not
+
 import uPPPP as p
 import time
 import numpy as np
 
 
+# add your function to this command list
+cmds=[]
 
-def test(data):
-    cmds=[p.Data.vectorfield]
+################## Vectorfield test #################
+def test_vectorfield(data):
+    grid = p.Grid.create2d(
+            (20,25),        # Number of cells
+            (-0.1,0.1),     # x-Dimensions
+            (-0.1,0.1)      # y-Dimensions
+        )
+    vx,vy,px,py=data.vectorfield(grid)
+    print(py.shape)
+    assert(vx.shape==(20,25)), "Wrong dimensions in vx-vectorfield"
+    assert(vy.shape==(20,25)), "Wrong dimensions in vy-vectorfield"
+    assert(px.shape==(20,25)), "Wrong dimensions in px-vectorfield"
+    assert(py.shape==(20,25)), "Wrong dimensions in py-vectorfield"
+cmds.append(test_vectorfield)
+##################### End ###############################
+
+
+def run_test(data):
     result=[]
     for cmd in cmds:
         t = time.time()
         print(f"Testing {cmd.__name__}")
         try:
-            result=cmd(data)
+            returndata=cmd(data)
         except KeyboardInterrupt:
             print("Ending program")
             raise KeyboardInterrupt
         except Exception as e:
+            print((
+            f"An error occured while executing function {cmd.__name__}\n"
+            f"ERROR: \n{e}"
+            ))
             with open(f"ERROR_{cmd.__name__}_{time.time()}.txt","w") as f:
                 f.write("uPPPP Error:\n"+str(e))
-            print(f"{cmd.__name__} failed after {time.time()-t} seconds")
+            print(f"{cmd.__name__} failed after {time.time()-t:.04f} seconds")
             result.append([cmd.__name__,time.time()-t,"No"])
             continue
-        print(f"{cmd.__name__} took {time.time()-t} seconds")
+        print(f"{cmd.__name__} succeeded after {time.time()-t:.04f} seconds")
         result.append([cmd.__name__,time.time()-t,"Yes"])
     return result
-    
 
-    
+
+
 if __name__=="__main__":
     print("Testing Simulation Data")
     data = p.Data.from_tdata("drum.hdf5")
-    test(data)
-    
-    print("Testing Experimental Data")
+    run_test(data)
+
+    print("\n\nTesting Experimental Data")
     data = p.Data.from_pdata("HSM_Glass_2l_250.hdf5")
-    test(data)
- 
- 
- 
- 
+    run_test(data)

63a9688a7ec7c0b4ea2c8fb970e75652b00ee3b6
Dominik Werner <d.wer2@gmx.de>
2021-11-24 15:00:55 +0000

    removing old minmax function

--- src/utilities/maths.rs
+++ src/utilities/maths.rs
@@ -12,10 +12,3 @@ pub fn minmax(arr: &Array1<f64>) -> (f64, f64) {
     }
     (min, max)
 }
-
-pub fn minmax2d(arr:&Array2<f64>)->(Array1<f64>, Array1<f64>){
-    let mut min = arr.map_axis(Axis(0),|view| *view.iter().min_by_key(|value| value).unwrap());
-    let mut max = arr.map_axis(Axis(0),|view| *view.iter().max().unwrap());
-
-    (min,max)
-}

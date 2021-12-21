c75913c02b938dce201a2465a1880fde220aeff2
Dan <daniel.weston@matthey.com>
2021-12-21 14:32:57 +0000

    added meshgrid

--- src/utilities/maths.rs
+++ src/utilities/maths.rs
@@ -13,6 +13,40 @@ pub fn minmax(arr: &Array1<f64>) -> (f64, f64) {
     (min, max)
 }
 
+pub fn meshgrid(
+    x: ndarray::Array1<f64>,
+    y: ndarray::Array1<f64>,
+) -> (ndarray::Array2<f64>, ndarray::Array2<f64>) {
+    let mut xx = ndarray::Array2::<f64>::zeros((x.len(), y.len()));
+    let mut yy = ndarray::Array2::<f64>::zeros((x.len(), y.len()));
+
+    for idx in 0..x.len() {
+        for idy in 0..y.len() {
+            xx[[idx, idy]] = x[idx];
+            yy[[idx, idy]] = y[idy];
+        }
+    }
+    return (xx, yy);
+}
+
+pub fn meshgrid3d(x: ndarray::Array1<f64>,
+    y: ndarray::Array1<f64>,
+    z: ndarray::Array1<f64>) -> (ndarray::Array3<f64>, ndarray::Array3<f64>, ndarray::Array3<f64>) {
+        let mut xx = ndarray::Array3::<f64>::zeros((x.len(),y.len(),z.len()));
+        let mut yy = ndarray::Array3::<f64>::zeros((x.len(),y.len(),z.len()));
+        let mut zz = ndarray::Array3::<f64>::zeros((x.len(),y.len(),z.len()));
+        for idx in 0..x.len() {
+            for idy in 0..y.len() {
+                for idz in 0..z.len() {
+                    xx[[idx, idy, idz]] = x[idx];
+                    yy[[idx, idy, idz]] = y[idy];
+                    zz[[idx, idy, idz]] = z[idz];
+                }
+            }
+        }
+        return (xx, yy, zz);
+    }
+
 /// Flattens a 2D array into a 1D array. 
 pub fn flatten_2d(arr:&Array2<f64>) -> Array1<f64>{
     return arr.slice(s![0..arr.shape()[0], 0..arr.shape()[1]]) //create slice of all elements

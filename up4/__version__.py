2e74028af0c50bed53850bcfc5908c0c7d1c027c
DanW97 <danw2697@btinternet.com>
2022-01-06 12:38:45 +0000

    dummy python class for docs

--- up4/__version__.py
+++ up4/__version__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
-#File    :   __init__.py
-#Time    :   2022/01/05 23:24:07
+#File    :   __version__.py
+#Time    :   2022/01/05 22:51:50
 #Author  :   Dominik Werner, Daniel Weston
 #Version :   0.1.0
 #Contact :   d.wer2@gmx.de, danw2697@btinternet.com
 #Licence :   GNU v3.0
 #Desc    :   None
-from .dummy import Dummy
-from upppp_rust import Data, Grid, Converter
 
 
-#TODO: see how to print string representations of rust python classes
+VERSION = (0, 1, 0)
+
+__version__ = '.'.join(map(str, VERSION))

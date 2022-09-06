6a00a40d0bc73adcf3cb166dad8c3c0452dbb572
Dan <dtw545@student.bham.ac.uk>
2022-09-06 17:46:14 +0100

    begin rework of plotting interface

--- src/plotting.rs
+++ src/plotting.rs
@@ -1,48 +1,56 @@
 //! Parent Module for plotting functions
 
 use colorous::Gradient;
-use ndarray::{Array, Array1};
-use plotly::{common::ColorScalePalette, Plot, Layout, layout::Axis};
+use dyn_clone::{DynClone, clone_trait_object};
+use ndarray::{Array1, Array2, Array3};
+use crate::GridFunctions3D;
 
-/// Traits for 2D and 3D data where:
-/// Type D is the array dimension
-/// Type P is the plot object
-pub trait VectorData<T, D, P> {
-    fn scale_global(&mut self, scale_factor: f64); 
-    fn scale_elementwise(&mut self, scale_array:Array<T, D>);
-    fn bound_min(&mut self, min: f64);
-    fn bound_max(&mut self, max: f64);
-    fn bound_min_max(&mut self, min: f64, max: f64);
-    fn bound_node(&mut self, dx: f64);
-    fn normalise_vectors(&mut self);
-    fn normalise_colour(&self, colour_bounds: Option<(f64, f64)>) -> (Array1<f64>, f64, f64);
-    fn create_plotly_traces(&self, arrow_scale: Option<f64>, colour: Gradient, colour_bounds: Option<(f64, f64)>) -> Vec<Box<P>>;
-    fn vector_plot(&self, traces: Vec<Box<P>>, layout: Layout, square: bool, axes: Vec<Option<Axis>>) -> Plot;
-    fn auto_axis_range(&self, layout: Layout, axes: Vec<Axis>, dtick: f64) -> Layout;
+use self::vector_plot::VectorPlotter;
+
+//FIXME doc
+pub mod vector_plot;
+/// Module for handling 2D scalar data
+pub mod scalar_plot;
+/// Module handling plots comparing 2 datasets
+pub mod comparison_plot;
+
+// FIXME doc
+pub fn axis_selector(grid: Box<dyn GridFunctions3D>, axis: usize) -> (Array1<f64>, Array1<f64>) {
+    match axis {
+        // yz view
+        0 => {
+            let xaxis = grid.get_ypositions().to_owned();
+            let yaxis = grid.get_zpositions().to_owned();
+            return (xaxis, yaxis)
+        }
+        // xz view
+        1 => {
+            let xaxis = grid.get_xpositions().to_owned();
+            let yaxis = grid.get_zpositions().to_owned();
+            return (xaxis, yaxis)
+        }
+        // xy view
+        2 => {
+            let xaxis = grid.get_xpositions().to_owned();
+            let yaxis = grid.get_ypositions().to_owned();
+            return (xaxis, yaxis)
+        }
+        // panic
+        _ => panic!("axis value must be either 0, 1 or 2!")
+    };
 }
 
-/// Module for creating 2D quiver plots
-pub mod vector2d;
+// FIXME doc
+pub fn data_selector(grid: Box<dyn GridFunctions3D>, axis: usize, index: usize) -> Array2<f64> {
+    let selected_data: Array2<f64> = grid.get_data().index_axis(ndarray::Axis(axis), index).into_owned();
+    return selected_data
+}
 
-/// Module for creating 3D cone plots
-pub mod vector3d;
-
-// Convenience functions that act on a plotly plot - for 3D we need to act on Scene instead
-// Manually set x axis range
-//pub fn axis_range_x(layout: &mut Layout, xaxis: Axis, xmin: f64, xmax:f64) {
-//    layout.x_axis(xaxis.range(vec![xmin, xmax]));
-//}
-
-// Manually set y axis range
-//pub fn axis_range_y(layout: &mut Layout, yaxis: Axis, ymin: f64, ymax:f64) {
-//    layout.y_axis(yaxis.range(vec![ymin, ymax]));
-//}
-
-// Manually set z axis range
-// pub fn axis_range_z(layout: &mut Layout, zaxis: Axis, zmin: f64, zmax:f64) {
-//    layout.z_axis(zaxis.range(vec![zmin, zmax]));
-// }
-
+//FIXME doc
+pub fn component_data_selector(data: Array3<f64>, axis: usize, index: usize) -> Array2<f64> {
+    let selected_data: Array2<f64> = data.index_axis(ndarray::Axis(axis), index).into_owned();
+    return selected_data
+}
 /*
 // Unit tests
 #[cfg(test)]

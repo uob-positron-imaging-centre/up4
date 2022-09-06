22553a7d62b96c9d92c5de0d347021716242f5c7
Dan <dtw545@student.bham.ac.uk>
2022-09-06 17:44:15 +0100

    begin example rework

--- examples/vector_plots.rs
+++ examples/vector_plots.rs
@@ -1,155 +1,15 @@
 use colorous::Gradient;
+use plotly::common::ColorBar;
 // This should appear in PR
-use plotly::common::{Mode, ColorBar, ColorScale, ColorScalePalette, Line, Marker, Fill, ColorScaleElement};
-use plotly::layout::{Margin, LayoutColorScale};
-use plotly::{Plot, Scatter, Layout};
+use plotly::common::{ColorScale::Palette, ColorScalePalette};
+use plotly::layout::{LayoutColorScale, Axis};
+use plotly::{Plot, Scatter, Layout, Trace};
 use ndarray::prelude::*;
-use upppp_rust::plotting::VectorData;
-use upppp_rust::plotting::vector2d::VectorData2D;
-use upppp_rust::plotting::vector3d::VectorData3D;
-use upppp_rust::utilities::maths::meshgrid3d;
-use upppp_rust::{plotting::vector2d, utilities::maths::meshgrid};
+use upppp_rust::{CartesianGrid3D, GridFunctions3D};
+use upppp_rust::plotting::vector_plot::VectorPlotter;
+use upppp_rust::utilities::maths::{meshgrid, meshgrid3d};
 use std::f64::consts;
-
-fn simple_color_scatter() {
-
-    let mut plot = Plot::new();
-    let gradient = colorous::VIRIDIS;
-    let x = vec![0.,1.,0.5,1.,1.];
-    let y = vec![0.,1.,1.,0.5,1.];
-    let c = 0.5; //pretend norm
-    let s = format!("#{:x}",gradient.eval_continuous(c));
-    let trace = Scatter::new(x,y)
-                        .mode(Mode::Lines)
-                        .fill(Fill::ToSelf)
-                        .fill_color(&s)
-                        .line(Line::new()
-                                    //.color_scale(ColorScale::Palette(ColorScalePalette::Viridis))
-                                    .color(s)
-                                    .cauto(true)) 
-                                    .show_legend(false);
-    plot.add_trace(trace);
-    let x = vec![0.,-2.,-1.,-2.,-2.];
-    let y = vec![0.,1.,1.,0.5,1.];
-    let c = 1.;
-    let s = format!("#{:x}",gradient.eval_continuous(c));
-    let trace = Scatter::new(x,y)
-                        .mode(Mode::Lines)
-                        .fill(Fill::ToSelf)
-                        .fill_color(&s)
-                        .line(Line::new()
-                        //.color_scale(ColorScale::Palette(ColorScalePalette::Viridis))
-                        .color(s)
-                        .cauto(true))
-                        .show_legend(false);
-    plot.add_trace(trace);
-    let x = vec![0.,0.];
-    let y = vec![0.5,1.];
-    let size: usize = 1;
-    let trace = Scatter::new(x,y)
-                        .mode(Mode::Markers)
-                        .marker(Marker::new()
-                                    .cmin(0.) //min norm
-                                    .cmax(1.) //max norm
-                                    .color_scale(ColorScale::Palette(ColorScalePalette::Viridis))
-                                    //.color(Rgb::new(0,0,0))
-                                    .color_bar(ColorBar::new())
-                        .size(size))
-                        .show_legend(false);
-    plot.add_trace(trace);
-    plot.use_local_plotly();
-    plot.show();
-}
-
-fn vector_2d(){
-    /* 
-    This example creates 4 vortices from the equations
-        u(x, y) =  sin(x)*cos(y)
-        v(x, y) = -sin(y)*cos(x)
-    where u and v are components of a vector at positions x and y.
-
-    In this example we see how to create the VectorData2D struct and 
-    visualise it as a quiver plot
-    */
-     
-    const PI: f64 = consts::PI;
-    const PTS: f64 = 60.; //number of points
-    
-    // create values for vortices
-    let x: Array1<f64> = Array::range(0.,2.*PI+PI/PTS,2.*PI/PTS);
-    let y: Array1<f64> = Array::range(0.,2.*PI+PI/PTS,2.*PI/PTS);
-    let (x, y) = meshgrid(x, y);                       
-    let u: Array2<f64> = &x.mapv(f64::sin)*&y.mapv(f64::cos);
-    let v: Array2<f64> = -&y.mapv(f64::sin)*&x.mapv(f64::cos);
-
-    // create VectorData2D struct
-    let mut arrows: VectorData2D = vector2d::VectorData2D::new(x,y,u,v);
-    arrows.scale_global(0.1);
-
-    // define properties for plot
-    let arrow_scale: Option<f64> = None; // default scaling
-    let colour: Gradient = colorous::VIRIDIS; // set colourmap
-    let colour_bounds: Option<(f64, f64)> = None; //Some((0.3, 0.5));
-    let traces: Vec<Box<Scatter<f64, f64>>> = arrows.create_plotly_traces(arrow_scale, colour, colour_bounds);
-    let layout = Layout::new()
-                        .width(1000)                
-                        .title("Quiver plot".into());
-    let square: bool = true;
-    let mut axes = Vec::new();
-    axes.resize_with(2, || None);
-    let plot: Plot = arrows.vector_plot(traces, layout, square, axes);
-    plot.show();
-}
-
-fn vector_3d(){
-    /* 
-    This example creates 4 vortices from the equations
-        u(x, y, z) =  sin(x)*cos(y)
-        v(x, y, z) = -sin(y)*cos(x)
-        w(x, y, z) = z
-    at different levels
-    where u, v and w are components of a vector at positions x, y and z.
-
-    In this example we see how to create the VectorData3D struct and 
-    visualise it as a cone plot
-    */
-     
-    const PI: f64 = consts::PI;
-    const PTS: f64 = 20.; //number of points
-    // TODO make this a sphere instead
-    // create values for vortices
-    let xs: Array1<f64> = Array::range(0.,PI+PI/PTS,PI/PTS);
-    let ys: Array1<f64> = Array::range(0.,2.*PI+PI/PTS,2.*PI/PTS);
-    let zs: Array1<f64> = Array::range(0.,2., 2./PTS);
-    let (x, y, z) = meshgrid3d(xs, ys, zs);                    
-    let u: Array3<f64> = &x.mapv(f64::sin)*&y.mapv(f64::cos);
-    let v: Array3<f64> = &y.mapv(f64::sin)*&x.mapv(f64::sin);
-    let w: Array3<f64> = x.clone().mapv(f64::cos);
-
-    // create VectorData2D struct
-    let mut cones: VectorData3D = VectorData3D::new(x,y,z,u,v,w);
-    //cones.scale_global(0.5);
-
-    // define properties for plot
-    let arrow_scale: Option<f64> = None; // default scaling
-    let colour: Gradient = colorous::VIRIDIS; // set colourmap
-    let colour_bounds: Option<(f64, f64)> = None; //Some((0.3, 0.5));
-    let traces = cones.create_plotly_traces(arrow_scale, colour, colour_bounds);
-    let layout = Layout::new()
-                        .width(1000)      
-                        .title("Cone plot".into())
-                        .color_scale(LayoutColorScale::new().diverging(
-                            ColorScale::Palette(ColorScalePalette::Viridis)
-                        )
-                            
-                        )
-                        ;
-    let square: bool = true;
-    let mut axes = Vec::new();
-    axes.resize_with(3, || None);
-    let plot: Plot = cones.vector_plot(traces, layout, square, axes);
-    plot.show();
-}
+use upppp_rust::grid::Dim;
 
 fn unit_vector_2d(){
     /* 
@@ -162,37 +22,57 @@ fn unit_vector_2d(){
     visualise it as a quiver plot, viewing the arrows as unit vectors
     and using a heatmap to colour the background based on the vector norms.
     */
-     
+    /* 
     const PI: f64 = consts::PI;
-    const PTS: f64 = 30.; //number of points
-    let dx: f64 = 2.0*PI/PTS; // spacing
+    const PTS: usize = 30; //number of points
     // create values for vortices
-    let x: Array1<f64> = Array::range(0.,2.*PI+dx,dx);
-    let y: Array1<f64> = Array::range(0.,2.*PI+dx,dx);
-    let (x, y) = meshgrid(x, y);                       
-    let u: Array2<f64> = &x.mapv(f64::sin)*&y.mapv(f64::cos);
-    let v: Array2<f64> = -&y.mapv(f64::sin)*&x.mapv(f64::cos);
-
+    // create the grids for the data
+    let limit: Dim = Dim::ThreeD([[0., 2.*PI], [0., 2.*PI], [0., 0.]]);
+    let cells: [usize; 3] = [PTS, PTS, 1];
+    let mut xgrid: Box<dyn GridFunctions3D> = Box::new(CartesianGrid3D::new(cells, limit));
+    let mut ygrid: Box<dyn GridFunctions3D> = xgrid.clone();
+    // assign values based on their positions
+    let posx = xgrid.get_xpositions().to_owned();
+    let posy = xgrid.get_ypositions().to_owned();
+    let posz = xgrid.get_zpositions().to_owned();
+    for i in 0..PTS {
+        for j in 0..PTS {
+            let xvalue = posx[i].sin()*posy[j].cos();
+            let yvalue = -posy[j].sin()*posx[i].cos();
+            xgrid.insert([posx[i], posy[j], posz[0]], xvalue);
+            ygrid.insert([posx[i], posy[j], posz[0]], yvalue);
+        }
+    }
     // create VectorData2D struct
-    let mut arrows: VectorData2D = vector2d::VectorData2D::new(x,y,u,v);
-    //arrows.bound_node(dx);
-    //arrows.normalise_vectors();
+    let mut arrows: VectorPlotter = VectorPlotter::new(&xgrid, &ygrid);
     // define properties for plot
     let uniform: bool = true;
     let arrow_scale: Option<f64> = None; // default scaling
-    let traces: Vec<Box<Scatter<f64, f64>>> = arrows.create_unit_plotly_traces(arrow_scale, uniform);
+    // create arrow traces
+    let mut traces: Vec<Box<dyn Trace>> = arrows.create_unit_vector_traces(arrow_scale, uniform);
+    // set layout
+    // FIXME layout settings
     let layout: Layout = Layout::new()
-                        .width(1000)                
-                        .title("Quiver plot".into());
+                        //.width(800)      
+                        //.height(height)
+                        .auto_size(true)         
+                        .title("2D vortex plot".into());
     let square: bool = true;
-    let mut axes = Vec::new();
-    axes.resize_with(2, || None);
-    let plot: Plot = arrows.unit_vector_plot(traces, layout, square, ColorScale::Palette(ColorScalePalette::Viridis), axes, None);
-    plot.show();
+    let xaxis: Option<Axis> = Some(Axis::new().title("x position".into()));
+    let yaxis: Option<Axis> = Some(Axis::new().title("y position".into()));
+    let axes = vec![xaxis, yaxis];
+    let smoothing = None;
+    let (heatmap, layout) = arrows.create_unit_vector_background(layout, square, axes, smoothing);
+    // adjust the heatmap colourbar and colourscale
+    let color_scale = Palette(ColorScalePalette::Viridis);
+    let color_bar = ColorBar::new().title("Vector magnitude".into()); 
+    traces.push(heatmap.color_scale(color_scale).zmax(1.).zmin(0.).color_bar(color_bar));
+    let show: bool = true;
+    let plot: Plot = arrows.plot(traces, layout, show);
+    */
 }
 
 fn main() {
-   //simple_color_scatter();
-   //vector_2d();
+
    unit_vector_2d();
 }

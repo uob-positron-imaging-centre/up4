d2ed0f56f5c10f57d2957424a18bf678b1d44465
Daniel Weston <56416455+DanW97@users.noreply.github.com>
2022-10-05 22:20:56 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ autodoc_default_options = {
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -198,7 +198,7 @@ html_last_updated_fmt = '%b %d, %Y'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied

dee76d0a2545c65b9565d5c8729daf564a15073d
Daniel Weston <56416455+DanW97@users.noreply.github.com>
2022-10-06 04:32:23 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -34,8 +34,8 @@
 
 import os
 import sys
-import up4
-sys.path.insert(0, os.path.abspath('../up4'))
+#import up4
+sys.path.insert(0, os.path.abspath('../__file__'))
 os.environ['MPLBACKEND'] = 'Agg'  # avoid tkinter import errors on rtfd.io
 
 

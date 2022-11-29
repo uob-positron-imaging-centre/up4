49e52edd5456d0cb50b1fb44f5d3a46a2aab6339
Dominik Werner <d.wer2@gmx.de>
2022-11-29 13:13:36 +0000

    New converter funktion, currently untested

---
+++ src/converter/csv_modes.rs
@@ -0,0 +1,6 @@
+#![allow(unused_variables, dead_code, unused_imports)]
+
+pub mod csv_multi_chain;
+pub mod csv_multi_idline;
+pub use csv_multi_chain::csv_multi_chain;
+pub use csv_multi_idline::csv_multi_idline;

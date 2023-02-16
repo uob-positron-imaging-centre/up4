5cbeecc4ea090667058a6321d01a630a112db579
dan <dtw545@student.bham.ac.uk>
2023-02-16 17:00:51 +0000

    more plotting interface stuff

--- up4/plotting/__init__.py
+++ up4/plotting/__init__.py
@@ -4,26 +4,12 @@
 # Time    :   02/09/2022
 # Author  :   Dominik Werner, Daniel Weston
 # Version :   0.1.0
-# Contact :   d.wer2@gmx.de, dtw545@bham.ac.uk
+# Contact :   dxw963@bham.ac.uk, dtw545@bham.ac.uk
 # Licence :   GNU v3.0
-# Desc    :   None
 
-from upppp_rust import VectorPlotter, ScalarPlotter, ComparisonPlotter
 from plotly.io import from_json
 import plotly
-
-
-def create_plot(self) -> plotly.graph_objects.Figure:
-    """
-    Converts generated JSON string into Plotly figure.
-
-    Returns
-    -------
-    plotly.graph_objects.Figure
-        Plotly figure object.
-    """
-    fig = from_json(self.plotting_string)
-    return fig
+from upppp_rust import Plotter2D
 
 
 def save_fig(self, fig: plotly.graph_objects.Figure, filename: str, dpi: int) -> None:
@@ -46,11 +32,24 @@ def save_fig(self, fig: plotly.graph_objects.Figure, filename: str, dpi: int) ->
     fig.write_image(filename, scale=scale)
 
 
-# add methods to classes
-ScalarPlotter.plot = create_plot
-VectorPlotter.plot = create_plot
-ComparisonPlotter.plot = create_plot
+class P2D(Plotter2D):
+    def __init__(self, grid):
+        Plotter2D._from_vector_grid(grid)
 
-ScalarPlotter.save_fig = save_fig
-VectorPlotter.save_fig = save_fig
-ComparisonPlotter.save_fig = save_fig
+    def quiver_plot(self, axis: int) -> plotly.graph_objects.Figure:
+        self._quiver_plot(axis)
+        quiver_plot = self._create_plot()
+
+        return quiver_plot
+
+    def _create_plot(self) -> plotly.graph_objects.Figure:
+        """
+        Converts generated JSON string into Plotly figure.
+
+        Returns
+        -------
+        plotly.graph_objects.Figure
+            Plotly figure object.
+        """
+        fig = from_json(self.plotting_string)
+        return fig

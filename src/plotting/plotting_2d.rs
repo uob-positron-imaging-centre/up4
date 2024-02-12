7bf84a6aa4cda86dde8bd1d13cc155cfa4529a5a
Daniel Weston <dtw545@student.bham.ac.uk>
2024-02-12 23:42:52 +0000

    unit tests

--- src/plotting/plotting_2d.rs
+++ src/plotting/plotting_2d.rs
@@ -1,5 +1,6 @@
 //! Submodule for handling 2D plots.
 /// Internal representation of arrow data
+#[derive(Clone, Debug)]
 struct Arrow {
     base: (f64, f64),
     tip: (f64, f64),
@@ -32,20 +33,16 @@ pub mod scalar_map;
 pub mod unit_vector;
 
 #[cfg(test)]
-mod test {
+pub(crate) mod plotting_test_support {
 
     use std::f64::consts;
 
     use ndarray::{Array1, Array3};
 
-    use crate::{
-        utilities::maths::meshgrid3d,
-        CartesianGrid3D, Dim, GridFunctions3D, VectorGrid,
-    };
-
+    use crate::{utilities::maths::meshgrid3d, CartesianGrid3D, Dim, GridFunctions3D, VectorGrid};
 
     // Helper functions
-    fn vector_grid() -> VectorGrid {
+    pub fn vector_grid() -> VectorGrid {
         let limit = Dim::ThreeD([[-2.0, 2.0], [-2.0, 2.0], [-2.0, 2.0]]);
         let n = 10;
         let cells = [n; 3];
@@ -62,19 +59,17 @@ mod test {
             2. * PI / ((n - 1) as f64),
         );
         let z = Array1::range(-1.0, 1.0 + 1.0 / ((n - 1) as f64), 2.0 / ((n - 1) as f64));
-        println!("{:?}", x.shape());
-        println!("{:?}", y.shape());
-        println!("{:?}", z.shape());
         let (xx, yy, _) = meshgrid3d(&x, &y, &z);
         let u = &xx.mapv(f64::sin) * &yy.mapv(f64::cos);
         let v = -&yy.mapv(f64::sin) * &xx.mapv(f64::cos);
-        let w = Array3::ones(cells) * 2.0;
+        let w = Array3::ones(cells) * 1.0;
+        let weights = w.clone();
         let mut ugrid = CartesianGrid3D::new(cells, limit.clone());
         let mut vgrid = CartesianGrid3D::new(cells, limit.clone());
         let mut wgrid = CartesianGrid3D::new(cells, limit);
-        println!("{:?}", u.shape());
-        println!("{:?}", v.shape());
-        println!("{:?}", w.shape());
+        ugrid.set_weights(weights.clone());
+        vgrid.set_weights(weights.clone());
+        wgrid.set_weights(weights);
         ugrid.set_data(u);
         vgrid.set_data(v);
         wgrid.set_data(w);
@@ -83,31 +78,6 @@ mod test {
         grid.data[1] = Box::new(vgrid);
         grid.data[2] = Box::new(wgrid);
 
-        println!("{:?}", grid);
-
         grid
     }
-    // fn direct_construction() -> QuiverPlot {}
-
-    // fn vector_grid() -> QuiverPlot {}
-
-    // Tests
-
-    // correct construction
-    #[test]
-    fn quiver_from_vector_grid() {
-        let grid = vector_grid();
-    }
-
-    // arrows created properly
-
-    // norm is adjusted properly
-
-    // scaling correctness
-
-    // colour range is determined properly
-
-    //
-
-    // traces are created properly
 }

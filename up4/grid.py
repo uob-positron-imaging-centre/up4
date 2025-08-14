9ff7c6356f7927d5a3bd0894486848991b55cc93
liliniklas007 <132018321+liliniklas007@users.noreply.github.com>
2025-08-14 10:49:16 +0200

    Update grid.py
    Fixed formatting based on ruff

--- up4/grid.py
+++ up4/grid.py
@@ -173,8 +173,8 @@ class Grid(RustGrid):
             elif data is None:
                 if limits is not None:
                     return self.cartesian3d(
-                            np.asarray(num_cells), np.asarray(limits)
-                        )
+                        np.asarray(num_cells), np.asarray(limits)
+                    )
                 elif xlim is not None and ylim is not None and zlim is not None:
                     dim = [xlim[0], xlim[1], ylim[0], ylim[1], zlim[0], zlim[1]]
                     return self.cartesian3d(np.asarray(num_cells), np.asarray(dim))
@@ -201,7 +201,7 @@ class Grid(RustGrid):
             elif data is not None and limits is not None:
                 return self.cylindrical3d(
                     np.asarray(num_cells, dtype=np.int64), np.asarray(limits)
-                    )
+                )
             # data is given and at least one of xlim,ylim,zlim is not none
             elif data is not None and (
                 xlim is not None or ylim is not None or zlim is not None
@@ -224,8 +224,8 @@ class Grid(RustGrid):
             elif data is None:
                 if limits is not None:
                     return self.cylindrical3d(
-                            np.asarray(num_cells, dtype=np.int64), np.asarray(limits)
-                        )
+                        np.asarray(num_cells, dtype=np.int64), np.asarray(limits)
+                    )
                 elif xlim is not None and ylim is not None and zlim is not None:
                     dim = [xlim[0], xlim[1], ylim[0], ylim[1], zlim[0], zlim[1]]
                     return self.cylindrical3d(
